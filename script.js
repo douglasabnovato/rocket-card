a831bac696e38c9e3416d39370a3f31d7b589a24
douglasabnovato <douglasabnovato@gmail.com>
2023-01-31 17:43:42 -0300

    botão randomico

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ console.log("Starting ...")
 const generate = document.querySelector(".box");
 
 function generatebkg() {
-    generate.style.background = "rgb(16, 159, 38 )";
+    let red = Math.floor(Math.random() * (255 - 0) + 0);
+    let green = Math.floor(Math.random() * (255 - 0) + 0);
+    let blue = Math.floor(Math.random() * (255 - 0) + 0);
+
+    console.log(red, green, blue)
+
+    generate.style.background = `rgb(${red}, ${green}, ${blue} )`;
 }
