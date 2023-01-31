f35ac3a94e6328b17b16a19e6808b972522f4d66
douglasabnovato <douglasabnovato@gmail.com>
2023-01-31 17:57:17 -0300

    telas

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-console.log("Starting ...")
-
 const generate = document.querySelector(".box");
 
 function generatebkg() {
     let red = Math.floor(Math.random() * (255 - 0) + 0);
     let green = Math.floor(Math.random() * (255 - 0) + 0);
     let blue = Math.floor(Math.random() * (255 - 0) + 0);
-
-    console.log(red, green, blue)
 
     generate.style.background = `rgb(${red}, ${green}, ${blue} )`;
 }
