bdedd848db7414b4bbdc099f3cad5adefca0f9ba
douglasabnovato <douglasabnovato@gmail.com>
2023-02-01 08:21:07 -0300

    detalhes para hoje

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const generate = document.querySelector(".box");
 
 function generatebkg() {
@@ -7,3 +9,24 @@ function generatebkg() {
 
     generate.style.background = `rgb(${red}, ${green}, ${blue} )`;
 }
+
+function getApi(url_github) {
+
+    let request = new XMLHttpRequest();
+
+    request.open("GET", url_github, false);
+    request.send();
+
+    return request.responseText;
+}
+
+function main() {
+    let data = getApi("https://api.github.com/users/douglasabnovato/repos");
+    let repositories = JSON.parse(data);
+    
+    let div_box_projects = document.getElementById("box-projects");
+    repositories.forEach((repository) => {
+      let repo = createRepo(repository);
+      div_box_projects.appendChild(repo);
+    });
+  }
